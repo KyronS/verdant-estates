5d287cb601e555cb24dd59ac10c579b3e2f9dbee
KyronS <49314173+KyronS@users.noreply.github.com>
2026-07-04 19:29:21 +0000

    Deploying to gh-pages from @ KyronS/verdant-estates@be176fbb27c5cb4c625f08224534d0e6ca17ebcf 🚀

---
+++ _next/static/chunks/app/(verdant)/projects/page-ed534d9a1f9ae73b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[708],{392:function(e,n,s){Promise.resolve().then(s.bind(s,8428)),Promise.resolve().then(s.bind(s,2448)),Promise.resolve().then(s.t.bind(s,8173,23)),Promise.resolve().then(s.t.bind(s,231,23))}},function(e){e.O(0,[740,231,173,741,215,971,23,744],function(){return e(e.s=392)}),_N_E=e.O()}]);
